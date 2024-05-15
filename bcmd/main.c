611c4ac0d399bae9c8b7977bc76d06f2e0a9a886
MVellosa <mvellosa@usp.br>
2024-05-14 21:49:12 -0300

    adding artur as coauthor and solving exercise 2

--- bcmd/main.c
+++ bcmd/main.c
@@ -1,40 +1,55 @@
 /*
  *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 ArturWeber
  *    SPDX-FileCopyrightText: 2024 MVellosa <mvellosa@usp.br>
  *   
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
- *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
- *  and contains modifications carried out by the following author(s):
- *  MVellosa <mvellosa@usp.br>
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
  */
 
 #include "bios.h"
 #include "utils.h"
 
-#define PROMPT "$ "		/* Prompt sign.      */
-#define SIZE 20			/* Read buffer size. */
+#define PROMPT "$"		/* Prompt sign.      */
+#define PROMPT_ROOT "#"		/* Prompt sign.      */
+#define SIZE 15			/* Read buffer size. */
 
 char buffer[SIZE];		/* Read buffer.      */
+char buffer_2[SIZE];
+char is_root = 0;
+#define PASSWORD "admin"
 
 int main()
 {
-  clear();
-  
-  println  ("Boot Command 1.0");
+  // clear();
 
   while (1)
     {
-      print(PROMPT);		/* Show prompt.               */
+      if (is_root)
+        print(PROMPT_ROOT" ");	/* Show prompt.               */
+      else
+        print(PROMPT" ");		/* Show prompt.               */
       readln(buffer);		/* Read use input.            */
 
       if (buffer[0])		/* Execute built-in command.  */
-	{
-	  if (!strcmp(buffer,"help"))
-	    println("A Beattles's song.");
-	  else 
-	    println("Unkown command.");
-	}
+        {
+          if (!strcmp(buffer,"su")){
+            println("password:");
+            readln(buffer_2);
+            if (!strcmp(buffer_2, PASSWORD)){
+              println("Hello root");
+              is_root = 1;
+            } else {
+              println("Denied!");
+            }
+          } else if (!strcmp(buffer,"end")){
+            if (is_root)
+              break;
+            else
+              println("Denied!");
+          }
+        }
     }
   
   return 0;
--- bcmd/utils.c
+++ bcmd/utils.c
@@ -1,12 +1,11 @@
 /*
  *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 ArturWeber
  *    SPDX-FileCopyrightText: 2024 MVellosa <mvellosa@usp.br>
  *   
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
- *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
- *  and contains modifications carried out by the following author(s):
- *  MVellosa <mvellosa@usp.br>
+ *    This file is part of SYSeg, available at https://gitlab.com/monaco/syseg.
  */
 
 int __attribute__((fastcall)) strcmp(const char *s1, const char *s2)
